3a62a396e823469e45ff0e915ae772483f17160f
JawnHo <771042322@qq.com>
2024-08-15 22:36:25 +0800

    修复国内centos下载kubeadm失败的问题

--- kainstall-centos.sh
+++ kainstall-centos.sh
@@ -1129,11 +1129,11 @@ function script::install_kube() {
   cat <<EOF > /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
-baseurl=https://pkgs.k8s.io/core:/stable:/v${repo}/rpm/
+baseurl=https://mirrors.aliyun.com/kubernetes/yum/repos/kubernetes-el7-x86_64/
 enabled=1
 gpgcheck=1
-gpgkey=https://pkgs.k8s.io/core:/stable:/v${repo}/rpm/repodata/repomd.xml.key
-exclude=kubelet kubeadm kubectl cri-tools kubernetes-cni
+repo_gpgcheck=0
+gpgkey=https://mirrors.aliyun.com/kubernetes/yum/doc/yum-key.gpg https://mirrors.aliyun.com/kubernetes/yum/doc/rpm-package-key.gpg
 EOF
 
   if [[ "${OFFLINE_TAG:-}" != "1" ]];then
